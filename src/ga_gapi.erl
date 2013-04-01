87cd2b48e7f959232c36124d3ae24eab6412e706
Alexey Aniskin <alexey.aniskin@gmail.com>
2013-04-01 10:56:58 +0600

    QPS shaper for requests

--- src/ga_gapi.erl
+++ src/ga_gapi.erl
@@ -11,9 +11,11 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {access_token= <<>>,
+-record(state, {access_token= <<>>, % refreshable google access token
+                requests=dict:new(), % http request / gen_server from refs pairs
+                shaper=[], % fixed-size list of timestamps
                 %% config values
-                key, secret, token, refresh_url, request_url, timeout, retry
+                key, secret, token, refresh_url, request_url, timeout, retry, qps
                }).
 
 %%%===================================================================
@@ -55,7 +57,6 @@ start_link() ->
 %% @end
 %%--------------------------------------------------------------------
 init([]) ->
-    inets:start(),
     ssl:start(),
     {ok, #state{key=?CFG(app_key),
                 secret=?CFG(app_secret),
@@ -63,7 +64,8 @@ init([]) ->
                 refresh_url=?CFG(refresh_url),
                 request_url=?CFG(request_url),
                 timeout=?CFG(http_timeout),
-                retry=?CFG(http_retry)}}.
+                retry=?CFG(http_retry),
+                qps=?CFG(qps)}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -96,13 +98,25 @@ handle_call(refresh_token, _From, State) ->
         {error, Err} -> {reply, {error, Err}, State}
     end;
 
-handle_call({request, Params}, _From, State) ->
+handle_call({request, Params}, From, #state{requests=Reqs, shaper=Shaper}=State) ->
     URL = State#state.request_url++binary_to_list(make_query(Params)),
-    %% @TODO what about "Bearer"
-    %% https://developers.google.com/analytics/devguides/reporting/core/v3/reference#data_request
     Headers = [{"Authorization", "OAuth "++binary_to_list(State#state.access_token)}],
-    Reply = http(get, {URL, Headers}, State),
-    {reply, Reply, State}.
+    Now = timestamp(),
+    %% queries per second shaper
+    if length(Shaper) < State#state.qps -> Shaper1 = [Now | Shaper];
+       true ->
+            {Shaper1, [Oldest]} = lists:split(State#state.qps, [Now | Shaper]),
+            Diff = Now - Oldest,
+            if Diff < 1000, Diff > 0 ->
+                    lager:warning("Delay request for ~b ms", [Diff]),
+                    timer:sleep(Diff);
+               true -> ok
+            end
+    end,
+    {ok, Req} = httpc:request(get, {URL, Headers}, [{timeout, State#state.timeout}],
+                              [{sync, false}, {full_result, false}]),
+    Reqs1 = dict:store(Req, From, Reqs),
+    {noreply, State#state{requests=Reqs1, shaper=Shaper1}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -127,8 +141,10 @@ handle_cast(_Msg, State) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-    {noreply, State}.
+handle_info({http, {Id, Res}}, #state{requests=Reqs}=State) ->
+    gen_server:reply(dict:fetch(Id, Reqs), Res),
+    Reqs1 = dict:erase(Id, Reqs),
+    {noreply, State#state{requests=Reqs1}}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -158,7 +174,7 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-%% @doc Simple http request wrapper. Only 200 code accepted. Retry on client error
+%% @doc Simple sync http request wrapper. Only 200 code accepted. Retry on client error
 http(Method, Request, State) -> http(Method, Request, State, State#state.retry).
 http(Method, Request, State, Attempt) -> 
     case httpc:request(Method, Request, [{timeout, State#state.timeout}], [{full_result, false}]) of
@@ -182,6 +198,11 @@ encode(Val) -> list_to_binary(http_uri:encode(binary_to_list(Val))).
 
 %% @doc Join list elements with divider
 join([H|T], Div) -> list_to_binary([H| [[Div, E] || E <- T]]).
+
+%% @doc Current timestamp, ms
+timestamp() ->
+    {M, S, Ms} = os:timestamp(), % no need to be as accurate as erlang:now/0 here
+    (M * 1000000 + S) * 1000 + Ms div 1000.
 
 %%%===================================================================
 %%% Unit tests
